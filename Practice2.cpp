d07dbc587e87b29ce73c8ff0c928a1b474c72f66
VitoScaleto <pushckaryovvitalya404@hmail.com>
2024-04-04 21:58:25 +0300

    minor changes

--- Practice2.cpp
+++ Practice2.cpp
@@ -6,22 +6,23 @@ using namespace std;
 void Printf(int& number, int& degree, int& primeNumber);// вывод
 bool isValidPrimeNumber(int validPrimeNumber);// проверка на простое число
 int FermsTheorema(int a, int p);// малая теорема ферма
-void PrintThFerma1(int& number, int& degree, int& primeNumber);
-void PrintThFerma2(int& number, int& degree, int& primeNumber);
+void PrintThFerma1(int& number, int& degree, int& primeNumber);// вывод теоремы ферма когда а кратно р
+void PrintThFerma2(int& number, int& degree, int& primeNumber);// вывод теоремы ферма когда а не кратно р
+int isValidDegree(int& degree);// проверка степени
 int main(void)
 {
 	setlocale(LC_ALL, "Rus");
 	int degree = 0, primeNumber = 0, number = 0;
 	Printf(number,degree,primeNumber);
-	if (degree == primeNumber)
+	if (degree == primeNumber)// используем теорему ферма
 	{
-		if (FermsTheorema(number,primeNumber) == 0)
+		if (FermsTheorema(number,primeNumber) == 0)// когда целое кратно простому
 		{
 			PrintThFerma1(number, degree, primeNumber);
 		}
 		else 
 		{
-			PrintThFerma2(number,degree,primeNumber);
+			PrintThFerma2(number,degree,primeNumber);//когда целое не кратно простому
 		}
 	}
 
@@ -33,8 +34,15 @@ void Printf(int& number, int& degree, int& primeNumber)// вывод
 {
 	cout << "Введите целое число: \nnumber = ";
 	cin >> number;
-	cout << "Введите степень, в которую возводится число number: \ndegree = ";
-	cin >> degree;
+	do
+	{
+		cout << "Введите степень, в которую возводится число number: \ndegree = ";
+		cin >> degree;
+		if (isValidDegree(degree) == false)
+		{
+			cout << "\nСтепень не может быть отрицательной!!!\n\n";
+		}
+	} while(isValidDegree(degree) == false);
 	do
 	{
 		cout << "Введите простое число primeNumber: \nprimeNumber = ";
@@ -45,7 +53,11 @@ void Printf(int& number, int& degree, int& primeNumber)// вывод
 		}
 	} while (isValidPrimeNumber(primeNumber) == false);
 }
-
+int isValidDegree(int& degree)
+{
+	if (degree == 0 || degree < 0) return false;
+	return true;
+}
 int FermsTheorema(int a, int p)// теорема ферма
 {
 	if (a % p == 0) return 0;
@@ -54,6 +66,7 @@ int FermsTheorema(int a, int p)// теорема ферма
 
 bool isValidPrimeNumber(int validPrimeNumber)// проверка на простое число
 {
+	if (validPrimeNumber == 0 || validPrimeNumber == 1) return false;
 	for (int i = 2; i < validPrimeNumber; i++)
 	{
 		if (validPrimeNumber % i == 0) return false;
@@ -62,11 +75,11 @@ bool isValidPrimeNumber(int validPrimeNumber)// проверка на прост
 	return true;
  }
 
-void PrintThFerma1(int& number, int& degree, int& primeNumber)
+void PrintThFerma1(int& number, int& degree, int& primeNumber)// вывод теоремы ферма когда а кратно р
 {
 	cout << "\nПо теореме Ферма:\n" << number << "^" << degree << " mod " << primeNumber << " = 0" << endl;
 }
-void PrintThFerma2(int& number, int& degree, int& primeNumber)
+void PrintThFerma2(int& number, int& degree, int& primeNumber)// вывод теоремы ферма когда а не кратно р
 {
 	cout << "\nПо теореме Ферма:\n" << number << "^" << degree << " mod " << primeNumber << " = " << number << "^" << degree - 1 << " mod " << primeNumber << " = 1";
 }
