e91908fd30ce41991ac2991708f86189020e0ad0
VitoScaleto <pushckaryovvitalya404@hmail.com>
2024-04-05 20:23:48 +0300

    changes

--- Practice2.cpp
+++ Practice2.cpp
@@ -4,70 +4,64 @@
 #include <string>
 using namespace std;
 
-void Printf (int& number, int& degree, int& primeNumber);       // вывод
-bool isValidPrimeNumber (int validPrimeNumber);                 // проверка на простое число
-int FermsTheorema (int a, int p);                               // малая теорема ферма
-void PrintThFerma (int& number, int& degree, int& primeNumber); // вывод теоремы ферма когда а кратно р
-int isValidDegree (int& degree);                                // проверка степени
-pair<int, int> remainderModLog (int& number, int& degree, int& primeNumber);
-string toBinary (int number);// translate from dec to bin
+void Printf(int& number, int& degree, int& deductionModule);       // scan and print
+bool isValidPrimeNumber(int validPrimeNumber);                 // проверка на простое число
+int FermsTheorema(int a, int p);                               // малая теорема ферма
+void PrintThFerma(int& number, int& degree, int& deductionModule); // вывод теоремы ферма
+pair<int, int> remainderModLog(int& number, int& degree, int& deductionModule);// algorithm log2
+string toBinary(int number);// translate from dec to bin
+void functionSwitchCase(int& number, int& degree, int& deductionModule);
 
-int main () 
+int main()
 {
-	int degree = 0, primeNumber = 0, number = 0;
-	Printf(number,degree,primeNumber);
-
-	if (degree == primeNumber) 
-	{ // используем теорему ферма
-		PrintThFerma( number, degree, primeNumber);
+	int degree = 0, deductionModule = 0, number = 0, number2 = 0;;
+	functionSwitchCase(number,degree,deductionModule);
+	if (degree == deductionModule && isValidPrimeNumber(deductionModule) == true)// проверка использования теоремы ферма
+	{
+		PrintThFerma(number, degree, deductionModule);
 	}
-
-	pair<int, int> result = remainderModLog(number, degree, primeNumber);
-	cout << "Result by model = " << result.first << " and by logarifm = " << result.second << endl;
+	pair<int, int> result = remainderModLog(number, degree, deductionModule);
+	cout << "\nResult by model = " << result.first << "\nResult By logarifm = " << result.second << endl;
 
 	return 0;
 }
 
-void Printf(int& number, int& degree, int& primeNumber) 
-{ // вывод
-	cout << "Input the data: \nnumber = ";
+void Printf(int& number, int& degree, int& deductionModule)// scan and print
+{ 
+	cout << "Input the number: \nnumber = ";
 	cin >> number;
 	do {
 		cout << "Input the degree: \ndegree = ";
 		cin >> degree;
-		if (isValidDegree(degree) == false)
+		if (degree < 0)
 		{
 			cout << "\nDegree cannot be subzero!!!\n\n";
 		}
-	} while(isValidDegree(degree) == false);
+	} while (degree < 0);
 	do {
-		cout << "Input prime number: \nprimeNumber = ";
-		cin >> primeNumber;
-		if (isValidPrimeNumber(primeNumber) == false)
+		cout << "Input deduction module: \ndeductionModule = ";
+		cin >> deductionModule;
+		if (deductionModule < 0)
 		{
-			cout << "\nThis is not a prime!!!\n\n";
+			cout << "\nDeduction module cannot be subzero!!!\n\n";
 		}
-	} while (isValidPrimeNumber(primeNumber) == false);
+	} while (deductionModule < 0);
 }
 
-int isValidDegree(int& degree)
-{
-	if (degree == 0 || degree < 0) return false;
-	return true;
-}
-
-int FermsTheorema(int a, int p) 
-{ // теорема ферма
-	if (a % p == 0) 
+int FermsTheorema(int a, int p)// теорема ферма
+{ 
+	if (a % p == 0)
 	{
 		return 0;
-	} else {
+	}
+	else 
+	{
 		return 1;
 	}
 }
 
-bool isValidPrimeNumber(int validPrimeNumber) 
-{ // проверка на простое число
+bool isValidPrimeNumber(int validPrimeNumber)// проверка на простое число
+{
 	if (validPrimeNumber == 0 || validPrimeNumber == 1) return false;
 	for (int i = 2; i < sqrt(validPrimeNumber) + 1; i++)
 	{
@@ -75,61 +69,86 @@ bool isValidPrimeNumber(int validPrimeNumber)
 
 	}
 	return true;
- }
-
-void PrintThFerma(int& number, int& degree, int& primeNumber)
-{ // вывод теоремы ферма когда а кратно р
-		cout << "\nBy Fermat's theorem:\n" << endl;
-		if (FermsTheorema(number,primeNumber) == 0) 
-		{          // когда целое кратно простому
-			cout << number << "^" << degree << " mod " << primeNumber << " = 0" << endl;
-		}
-		else 
-		{    //когда целое не кратно простому
-			cout << number << "^" << degree << " mod " << primeNumber << " = " << number << "^" << degree - 1 << " mod " << primeNumber << " = 1";
-		}
 }
 
-pair<int, int> remainderModLog(int& number, int& degree, int& primeNumber)
+void PrintThFerma(int& number, int& degree, int& deductionModule)// вывод теоремы ферма
+{ 
+	cout << "\nBy Ferm's theorem:\n" << endl;
+	if (FermsTheorema(number, deductionModule) == 0)// когда целое кратно простому
+	{          
+		cout << number << "^" << degree << " mod " << deductionModule << " = 0" << endl;
+	}
+	else //когда целое не кратно простому
+	{    
+		cout << number << "^" << degree << " mod " << deductionModule << " = " << number << "^" << degree - 1 << " mod " << deductionModule << " = 1";
+	}
+}
+
+pair<int, int> remainderModLog(int& number, int& degree, int& deductionModule)
 {
-	pair<int,int> result = {1,1};
+	pair<int, int> result = { 1,1 };
 
-    for (int i = 1; i <= degree; i++)
+	for (int i = 1; i <= degree; i++)
 	{
-        result.first *= number;
-        result.first %= primeNumber;
-    }
-
+		result.first *= number;
+		result.first %= deductionModule;
+	}
 	int logByNumber = log2(number);
 	string binary = toBinary(number);
 	vector<int> numForResult;
 	for (int i = 0; i <= logByNumber; i++)
 	{
-		numForResult.push_back(pow(number, pow(2,i)));
+		numForResult.push_back(pow(number, pow(2, i)));
 	}
-	int resultForPrime = 1;
-	for (int i = 0; i< binary.length(); i++)
+	int resultForPrime = 0;
+	for (int i = 0; i < binary.length(); i++)
 	{
-		if (binary[i] == '1'){
+		if (binary[i] == '1') {
 			resultForPrime *= numForResult[i];
 		}
 	}
-	result.second = resultForPrime % primeNumber;
+	result.second = resultForPrime % deductionModule;
 
 	return result;
 }
 
-string toBinary (int number) 
+string toBinary(int number) // translate from dec to bin
 {
-    if (number == 0) {
-        return "0";
-    }
+	if (number == 0) {
+		return "0";
+	}
+	string binary = "";
+	while (number > 0) {
+		binary = to_string(number % 2) + binary;
+		number /= 2;
+	}
 
-    string binary = "";
-    while (number > 0) {
-        binary = to_string(number % 2) + binary;
-        number /= 2;
-    }
+	return binary;
+}
 
-    return binary;
+void propertiesOfComparisons(int& number, int& degree, int& deductionModule, int& number2)//свойства сравнений
+{
+	cout << "Enter number "
+	Printf(number, degree, deductionModule);
+
+}
+void functionSwitchCase(int& number, int& degree, int& deductionModule)
+{
+	cout << "To select module comprasion properties press 1\n";
+	cout << "To selecte everything else press 2\n";
+	cout << "Enter number: ";
+	char s;
+	cin >> s;
+	switch (s) {
+
+	case('1'):
+		propertiesOfComparisons();
+		break;
+	case ('2'):
+		Printf(number, degree, deductionModule);
+		break;
+	default:
+		cout << "Error";
+		break;
+	}
 }
